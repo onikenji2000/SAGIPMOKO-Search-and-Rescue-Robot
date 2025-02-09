d757e03fbba9b4e1e72745b470a9d044f2afc770
Charles Louie Siplao <133845279+onikenji2000@users.noreply.github.com>
2025-02-10 08:44:35 +1100

    Update sagipmoko_rev1.ino

--- sagipmoko_rev1/sagipmoko_rev1.ino
+++ sagipmoko_rev1/sagipmoko_rev1.ino
@@ -173,9 +173,13 @@ void cameraControl(int rotx, int roty) {
 }
 void temperature()
 {
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
   Serial.print("Requesting temperatures...");
   sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
   float tempC = sensors.getTempCByIndex(0);
 
   // Check if reading was successful
